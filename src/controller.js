cbb8821557eb164b981d42d5a4286d6f14708ab7
Loruel <paco.jeansl@gmail.com>
2024-07-08 01:32:07 -0600

    070724 third commit

--- src/controller.js
+++ src/controller.js
@@ -51,12 +51,29 @@ export const exportCsv = async(request, response) => {
     }
 }
 
-
-
 export const importCsv = async(request, response) => {
     try {
+        const ruta = path.resolve('./public/usuarios.txt')
+        const contenido = (await fs.readFile (ruta, 'utf-8')).split('\n')
+        const cabecera = contenido[0].split(',')
+        const lineas = contenido.slice(1)
+
+        console.log('Número de líneas a procesar:', cabecera)
+
+        for (const linea of lineas){
+            const datos = linea.split(',')
+            const sql = `INSERT INTO datos(ID,NOMBRES,APELLIDOS,DIRECCION,MAIL,DNI,EDAD,CREACION,TELEFONO)VALUES (?,?,?,?,?,?,?,?,?)`
+
+            await pool.query(sql, datos)
+ 
+            /* await pool.query(INSERT INTO datos(`${cabecera[0]},${cabecera[1]},${cabecera[2]},${cabecera[3]},${cabecera[4]},${cabecera[5]},${cabecera[6]},${cabecera[7]},${cabecera[8]} VALUES (?,?,?,?,?,?,?,?,?)`, dato )) */
+        }
         
+        response.writeHead(200, {'Content-Type': 'application/JSON: charset=utf-8'})
+        response.end(JSON.stringify({message: `Usuarios importados a ${ruta}`}))
     } catch (error) {
-        
-    }
-}
+        console.error('Error al importar la tabla', error)
+        response.writeHead(500, {'Content-Type': 'text/plain;'})
+        response.end(JSON.stringify({message:'Error en el servidor'}))
+     }
+}   
